c21739d6576bc47ec1be1e4fe7d2470f0e221b20
Clemens Wagner <wagner.clemens@gmx.de>
2020-01-05 19:40:42 +0100

    added daily report, https => axions

--- main.js
+++ main.js
@@ -1,18 +1,30 @@
 const bitcoin = require('bitcoin-address-generator');
-const https = require('https'); 
+const axios = require('axios'); 
 const asciiart = require('figlet');
 const fs = require('fs');
-const api = "https://api-r.bitcoinchain.com/v1/address/";
+
+const API = "https://api-r.bitcoinchain.com/v1/address/";
+const ONE_DAY = 86400000;
+
 let count = 0;
+let dailyCount = 0;
+let totalCount = 0;
 main();
 
-function main()
+async function main()
 {
     console.log(asciiart.textSync("thi3fmin32"));
-    setInterval(() => run(), 1010);
+    await postSlack("starting \n```" + asciiart.textSync("thi3fmin32") + "```");
+    setInterval(async () => await run(), 1010);
+
+    setTimeout(async () => 
+    {
+        setInterval(async () => await report(), ONE_DAY);
+        await report();
+    }, getTimeTillXh(20)); 
 }
 
-function run()
+async function run()
 {
     count++;
     log("starting run " + count);
@@ -20,13 +32,85 @@ function run()
     for (let i = 0; i < 100; i++)
         bitcoin.createWalletAddress((w) => { wallets.push(w) });
     
-    checkBalance(wallets);
+    await checkBalance(wallets);
 }
 
-function checkBalance(wallets)
+async function checkBalance(wallets)
 {
-    let url = api + wallets.map(w => w.address).join(',');
-    let request = https.get(url)
+    let url = API + wallets.map(w => w.address).join(',');
+    try
+    {
+        const response = await axios.get(url);
+        if (response.data.length != wallets.length)
+            error("missmatch between results and wallets");
+
+        dailyCount += wallets.length;
+        for (let i = 0; i < wallets.length; i++)
+        {
+            let wallet = wallets[i];
+            let balance = response.data[i].balance ? response.data[i].balance : 0;
+            log("address: " + wallet.address + " balance: " + balance);
+            if (balance > 0)
+            {
+                console.log(asciiart.textSync("JACKPOT"));
+                log("found: " + balance + " on address " + wallet.address + " with private key " + wallet.key);
+                fs.appendFileSync('jackpot.csv', "" + wallet.key + ";" + wallet.address + ";" + balance);
+                await postSlack("```" + ciiart.textSync("JACKPOT") + "```\n" + balance + " BTC");
+            }
+        }
+    }
+    catch (e)
+    {
+        error("requesting balance failed:" + e.message);
+    }
+}
+
+function log(msg)
+{
+    let date = new Date();
+    let time = "[" + date.toLocaleDateString() + " " + date.toLocaleTimeString() + "] ";
+    console.log(time + msg);
+}
+
+function error(msg)
+{
+    let date = new Date();
+    let time = "[" + date.toLocaleDateString() + " " + date.toLocaleTimeString() + "] ";
+    console.error(time + msg);
+}
+
+async function postSlack(msg)
+{
+    try
+    {
+        let payload = { "username": "thi3fmin32", "text": msg}
+        await axios.post("https://hooks.slack.com/services/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", payload);     
+    }
+    catch (e)
+    {
+        error("failed posting to slack channel");
+    }
+}
+
+async function report()
+{
+    totalCount += dailyCount;
+    await postSlack("Checked " + dailyCount + " adresses today, totally " + totalCount + "\n =>" + (totalCount / Math.pow(2, 160)( + "%")));
+    dailyCount = 0;
+}
+
+function getTimeTillXh(x)
+{
+    var now = new Date();
+    var todayX = new Date();
+    todayX.setHours(x,0,0,0);
+    let timespan = todayX - now;
+    if (timespan < 0)
+        timespan + ONE_DAY;
+
+    return timespan;
+}
+/*
 
     request.on('error', (err) => 
     {
@@ -60,30 +144,15 @@ function checkBalance(wallets)
                     {
                         console.log(asciiart.textSync("JACKPOT"));
                         fs.appendFileSync('jackpot.csv', "" + wallet.key + ";" + wallet.address + ";" + balance);
+                        https.post("https://hooks.slack.com/services/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", 
                     }
                 }
             } 
             catch (e) 
             {
-                error("requesting balance failed:" + e.message);
             }
         });
 
         request.end();
 
-    });
-}
-
-function log(msg)
-{
-    let date = new Date();
-    let time = "[" + date.toLocaleDateString() + " " + date.toLocaleTimeString() + "] ";
-    console.log(time + msg);
-}
-
-function error(msg)
-{
-    let date = new Date();
-    let time = "[" + date.toLocaleDateString() + " " + date.toLocaleTimeString() + "] ";
-    console.error(time + msg);
-}
+    });*/
